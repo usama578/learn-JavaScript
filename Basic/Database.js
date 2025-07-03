d272c0a8322a9794d227cc374216276dd6826f80
Usama Mayo <usamamayo0098123@gmail.com>
2025-07-03 18:21:07 +0000

    Datatypes

---
+++ Basic/Database.js
@@ -0,0 +1,25 @@
+"Use Strict"
+
+//alert (3 + 3) we are using node js , not browser
+
+console.log("Usama");
+
+let name = "Usama"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//// number => 2 to power 53
+// bigint
+// strict => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+
+// object  
+
+console.log(typeof undefine); // undefine
+
+console.log(typeof null); // object
