31718844ea7f302d4b3a5754b460646a4ffdade9
Usama Mayo <usamamayo0098123@gmail.com>
2025-07-05 19:40:50 +0000

    Prefix and Postfix

---
+++ Basic/opration.js
@@ -0,0 +1,26 @@
+let Value = 3
+let nagValue = -Value
+ //console. log(nagValue)
+
+let str1 = "Usama"
+let str2 = " Muneeb"
+
+let str3 = str1 + str2
+ //console.log(str3);
+
+ //console.log(+true);
+// console.log(+"");
+
+
+ let gameCounter = 100
+ ++gameCounter;
+ // console.log(gameCounter);
+ 
+
+ let Usama = 100
+ Usama++
+// console.log(Usama);
+ 
+let a = 3
+a++
+console.log(a);
