fc99f3e6afd1c74f1b0d1744ec754a2d78f5ac4a
Usama Mayo <usamamayo0098123@gmail.com>
2025-07-08 17:51:29 +0000

    copmrison

---
+++ Basic/comperison.js
@@ -0,0 +1,14 @@
+// console. log(2 > 1)
+
+
+// console. log("2" > 1)
+// console. log("02" > 1)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); yeh comperison hame parashani ma dal sakya ha..
+
+// ===
+
+console.log();
+
